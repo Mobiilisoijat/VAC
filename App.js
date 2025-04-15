8d227a774e78a850817d8cd5862ce04915313ed4
Arttu <arttuiivari1@outlook.com>
2025-04-15 18:13:41 +0300

    Added QR-scanner with minimalistic 'authentication' while updating arduino state

--- App.js
+++ App.js
@@ -1,18 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Alert, Button, StyleSheet, Text, View } from 'react-native';
 import axios from "axios"
+import { Camera, CameraView } from 'expo-camera';
 
 const esp12sUrl = "http://192.168.4.1"
 
 export default function App() {
 
-  const [status, setStatus] = useState("Unknown")
+  const [status, setStatus] = useState("Unknown") //Status of arduino side
+  // Camera reading functionality
 
-  const sendCommand = async(command) => {
+  const [hasPermission, setHasPermission] = useState(null)
+  const [scanned, setScanned] = useState(false)
+  const [data, setData] = useState('')
+
+  
+
+  const askForCameraPermission = async () => {
+    const { status } = await Camera.requestCameraPermissionsAsync()
+    setHasPermission(status === 'granted')
+  }
+  useEffect(() => {
+    askForCameraPermission()
+  }, [])
+
+  const handleQRCodeScan = ({type, data}) => {
+    setScanned(true)
+    console.log(`Type: ${type} \nData: ${data}`)
+    if(data.length == 28 && type == "qr"){  //Without internet access, this is enough for now (user UID length)
+      setData(data)
+      console.log("QR-koodinluku onnistui!") //Motor should turn on: change command back to OFF when a certain time passes on arduino side
+      sendCommand("ON") // and handle turning motor so the gate is closed again
+      
+    } else{
+      setData("Vääränlainen tieto luettu:"+data)
+    }
+    setTimeout(() => {setScanned(false)}, 3000)
+  }
+
+  // Command sending functionality for arduino via WiFi connection
+  const sendCommand = async (command) => {
     try {
       const response = await axios.post(esp12sUrl, `command=${command}`, {
-        headers:{
+        headers: {
           "Content-Type": "application/x-www-form-urlencoded",
         },
         timeout: 5000,
@@ -21,32 +52,61 @@ export default function App() {
 
       setStatus(response.data)
 
-    }catch(error){
-      if(axios.isAxiosError(error)) {
-        if(error.code === "ECONNABORTED") {
-          Alert.alert("Error","The request took too long. Please check your connection to ESP.")
-        } else if(error.code === "Network Error") {
-          Alert.alert("Error","Could not connect to esp. Please check your connection to ESP.")
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        if (error.code === "ECONNABORTED") {
+          Alert.alert("Error", "The request took too long. Please check your connection to ESP.")
+        } else if (error.code === "Network Error") {
+          Alert.alert("Error", "Could not connect to esp. Please check your connection to ESP.")
         } else {
-          Alert.alert("Error","There was a problem communicating with ESP.")
+          Alert.alert("Error", "There was a problem communicating with ESP.")
         }
 
       } else {
-        Alert.alert("Error","An unexpected error occured.")
+        Alert.alert("Error", "An unexpected error occured.")
       }
     }
 
   }
 
+  // handle permissions for camera usage
+  if(hasPermission === null) {
+    return(
+      <View style={styles.container}>
+        <Text>Pyydetään lupaa käyttää kameraa</Text>
+      </View>
+    )
+  }
+  if(hasPermission === false) {
+    return(
+      <View style={styles.container}>
+        <Text>Ei lupaa käyttää kameraa</Text>
+        <Button onPress={() => askForCameraPermission()} title="Pyydä lupaa käyttää kameraa"/>
+      </View>
+    )
+  }
+  // Camera view for scanning and buttons to manually alter accessgate's state
   return (
     <View style={styles.container}>
-      <Text>Paina nappeja</Text>
+
+      <View>
+        <CameraView 
+          onBarcodeScanned={scanned ? undefined : handleQRCodeScan}
+          style={{height: 500, width: 500}}
+        />
+      </View>
+      <Text>{data}</Text>
+
+
+
+      
+      <Text>Päivitä portin asento manuaalisesti</Text>
 
       <Button title="ON" onPress={() => sendCommand("ON")} />
       <Button title="OFF" onPress={() => sendCommand("OFF")} />
 
       <Button title="Check status" onPress={() => sendCommand("STATE")} />
-      <Text>Lamp status: {status}</Text>
+      <Text>Pääsyportin tila: {status}</Text>
       <StatusBar style="auto" />
     </View>
   );
